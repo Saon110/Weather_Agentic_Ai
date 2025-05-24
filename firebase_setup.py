9313938550e6de1b0aa12306945e7719c01411a0
Sijon Chisty Saon <2105110@ugrad.cse.buet.ac.bd>
2025-05-24 22:21:01 +0600

    last

--- firebase_setup.py
+++ firebase_setup.py
@@ -11,24 +11,3 @@ def init_firestore():
 
 
 
-# // Import the functions you need from the SDKs you need
-# import { initializeApp } from "firebase/app";
-# import { getAnalytics } from "firebase/analytics";
-# // TODO: Add SDKs for Firebase products that you want to use
-# // https://firebase.google.com/docs/web/setup#available-libraries
-
-# // Your web app's Firebase configuration
-# // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-# const firebaseConfig = {
-#   apiKey: "AIzaSyByqvVr04fryNuMlsxLu5PpU5GxaD4iauY",
-#   authDomain: "weather-ai-agent-1c82f.firebaseapp.com",
-#   projectId: "weather-ai-agent-1c82f",
-#   storageBucket: "weather-ai-agent-1c82f.firebasestorage.app",
-#   messagingSenderId: "737206906452",
-#   appId: "1:737206906452:web:2472ce19cce68d63da87c1",
-#   measurementId: "G-FCH9N2LXNZ"
-# };
-
-# // Initialize Firebase
-# const app = initializeApp(firebaseConfig);
-# const analytics = getAnalytics(app);
