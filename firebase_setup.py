0cee4529f34c5f676e52c17f7faee7f28f8bf2e5
Sijon Chisty Saon <2105110@ugrad.cse.buet.ac.bd>
2025-05-24 22:38:40 +0600

    modified dotenv

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,10 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def init_firestore():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("weather-ai-agent-1c82f-firebase-adminsdk-fbsvc-6133cd3cea.json")  # Download this from Firebase Console
+        cred = credentials.Certificate({
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+    })
         firebase_admin.initialize_app(cred)
     return firestore.client()
 
